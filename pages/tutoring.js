6024a4dfef954758ed0c65f3db10df274fef603a
Arman Babakhani <armanxbabakhani@berkeley.edu>
2022-05-17 08:42:16 +0000

    added some descriptions to the tutoring page.

--- pages/tutoring.js
+++ pages/tutoring.js
@@ -15,6 +15,16 @@ export default function Tutoring() {
                 <h2>
                     Arman Babakhani | Future Tutoring
                 </h2>
+                <p className={styles.plaintext}>
+                    Private tutoring for subjects of mathematics, physics, and chemistry is offered. Here is a detailed list:
+                    <li> High School: AP Physics, Calculus and Chemistry</li>
+                    <li> Physics: Quantum and Classical mechanics both undergraduate and graduate level, Electromagnetism, quantum computing and more</li>
+                    <li> Calculus and higher mathematics: multivariable/vector calculus, complex analysis, linear algebra and more</li>
+                    <li> Chemistry: Organic, Inorganic, physical, and quantum chemistry undergraduate and some graduate level </li>
+                </p>
+            </p>
+            <p className={styles.main}>
+                <h2> My Experience in Teaching </h2>
             </p>
             
             <footer className={styles.footer}>
