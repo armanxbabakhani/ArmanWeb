d5e251d330ca8ebc23d2dc4923cd777c5394d300
Arman Babakhani <armanxbabakhani@berkeley.edu>
2023-02-06 18:20:27 +0000

    updated google analytics script tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,21 @@
 import '../styles/globals.css'
 import Layout from "../component/layout"
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   return( 
       <Layout>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-11087720521"></Script>
+        <Script>
+          {
+            `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+  
+            gtag('config', 'AW-11087720521');
+            `
+          }
+        </Script>
         <Component {...pageProps} /> 
       </Layout>
 )
--- pages/index.js
+++ pages/index.js
@@ -5,64 +5,66 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <html>
       <Head>
         <title>Arman Babakhani</title>
         <meta name="personal website" content="porjects and services, initial-scale=1.0" />
         <link rel="icon" href="/Armanlogo.png" />
       </Head>
-
-      <main className={styles.main}>
-        <div className={styles.pframe}>
-          <Image src="/Arman_portrait_1.jpg" alt="Arman" width="340px" height="290px" className={styles.portrait}/>
-        </div>
-        <div className={styles.introbox}>
-          <h3>Welcome! I am</h3>
-          <h1>
-            Arman Babakhani 
-          </h1>
-          <h4>I am a researcher and a PhD student in quantum information theory and quantum algorithms.</h4>
-        </div>
-        <div className={styles.grid}>
-          {' '}
-          <div  className={styles.card}>
-            <Link href="/me"passHref>
-              <a>
-                <h2>About Me &rarr;</h2>
-                <p> My educational background and interests.</p>
-              </a>
-            </Link>
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <div className={styles.pframe}>
+            <Image src="/Arman_portrait_1.jpg" alt="Arman" width="340px" height="290px" className={styles.portrait}/>
           </div>
-
-          <div className={styles.card}>
-            <Link href="/projects"passHref>
-              <a>
-                <h2>Research &rarr;</h2>
-                <p>Completed projects and collaborations</p>
-              </a>
-            </Link>
+          <div className={styles.introbox}>
+            <h3>Welcome! I am</h3>
+            <h1>
+              Arman Babakhani 
+            </h1>
+            <h4>I am a researcher and a PhD student in quantum information theory and quantum algorithms.</h4>
           </div>
+          <div className={styles.grid}>
+            {' '}
+            <div  className={styles.card}>
+              <Link href="/me"passHref>
+                <a>
+                  <h2>About Me &rarr;</h2>
+                  <p> My educational background and interests.</p>
+                </a>
+              </Link>
+            </div>
 
-          <div className={styles.card}>
-            <Link href="/tutoring" passHref>
-              <a>
-                <h2>Tutoring &rarr;</h2>
-                <p>Tutoring and educational consultation for STEM students.</p>
-              </a>
-            </Link>
+            <div className={styles.card}>
+              <Link href="/projects"passHref>
+                <a>
+                  <h2>Research &rarr;</h2>
+                  <p>Completed projects and collaborations</p>
+                </a>
+              </Link>
+            </div>
+
+            <div className={styles.card}>
+              <Link href="/tutoring" passHref>
+                <a>
+                  <h2>Tutoring &rarr;</h2>
+                  <p>Tutoring and educational consultation for STEM students.</p>
+                </a>
+              </Link>
+            </div>
+
+            <a
+              href="#!"
+              className={styles.card}
+            >
+              <h2>Notes and Ongoing Projects &rarr;</h2>
+              <p>
+                Some of my ongoing projects and some notes on miscellaneous topics.
+              </p>
+            </a>
           </div>
-
-          <a
-            href="#!"
-            className={styles.card}
-          >
-            <h2>Notes and Ongoing Projects &rarr;</h2>
-            <p>
-              Some of my ongoing projects and some notes on miscellaneous topics.
-            </p>
-          </a>
-        </div>
-      </main>
-    </div>
+        </main>
+      </div>
+    </html>
+    
   )
 }
